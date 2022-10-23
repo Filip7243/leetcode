0699916b2108ffe83f929d48ca9551e973e78884
Filip7243 <filip7243@gmail.com>
2022-10-23 12:46:14 +0200

    better solution for palinrome

--- src/PalindromeNumber.java
+++ src/PalindromeNumber.java
@@ -1,7 +1,20 @@
 public class PalindromeNumber {
 
     public static void main(String[] args) {
-        System.out.println(isPalindrome(111));
+        System.out.println(betterIsPalindrome(194));
+    }
+
+    // another solution
+    private static boolean betterIsPalindrome(int x) {
+        int temp = x;
+        int y = 0;
+
+        while(x > 0) {
+            y = y * 10 + x % 10;
+            x /= 10;
+        }
+
+        return temp == y;
     }
 
     private static boolean isPalindrome(int x) {
